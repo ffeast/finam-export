944a960482706c5978de6f99902fb8bb9bd8f77a
ffeast <ffeast@gmail.com>
2020-09-12 23:47:47 +0300

    * bumped version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # coding: utf8
 from setuptools import setup
 
-VERSION = '3.0.0'
+VERSION = '3.0.1'
 
 long_description = open('README.md').read()
 
@@ -15,7 +15,7 @@ setup(name='finam-export',
       author='ffeast',
       author_email='ffeast@gmail.com',
       url='https://github.com/ffeast/finam-export',
-      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
+      python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, <4',
       install_requires=[
           'pandas>=1.1.2',
           'requests>=2.24.0, <3',
